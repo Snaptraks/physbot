62a7ca746c5a35e6dc8dccb4b923afa6f10d6755
François Hardy <snaptraks@gmail.com>
2020-09-15 14:54:34 -0400

    Add random quote

--- cogs/physum.py
+++ cogs/physum.py
@@ -1,10 +1,15 @@
 import discord
 from discord.ext import commands
+import json
+import random
 
 
 class Physum(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+        with open('physics_quotes.json') as f:
+            self.quotes = json.load(f)
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
@@ -17,6 +22,19 @@ class Physum(commands.Cog):
                 'pour avoir accès à plus de salons et de fun :) '
             )
             await guild.system_channel.send(to_send)
+
+    @commands.Cog.listener(name='on_message')
+    async def quote(self, message):
+        """Provide a random physics quote when mentionned."""
+
+        if (self.bot.user.mentioned_in(message)
+                and not message.mention_everyone
+                and not message.content.startswith(self.bot.command_prefix)):
+
+            quote = random.choice(self.quotes)
+            content = f"{quote['text']}\n — *{quote['author']}*"
+
+            await message.channel.send(content)
 
 
 def setup(bot):
