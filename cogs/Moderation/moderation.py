2d5063adc64851a3b73474b30aad3868908dd9fd
Snaptraks <snaptraks@gmail.com>
2021-02-11 13:38:40 -0500

    Initial commit to Moderation cog

---
+++ cogs/Moderation/moderation.py
@@ -0,0 +1,10 @@
+from discord.ext import commands
+
+
+class Moderation(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    # TODO: add edited message log
+
+    # TODO: add deleted message log
