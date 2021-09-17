68aaaa7affdfa8602081d699f5db01169c7103f8
Snaptraks <snaptraks@gmail.com>
2021-09-17 11:57:36 -0400

    Use asyncpraw to fetch reddit submissions

--- cogs/memes.py
+++ cogs/memes.py
@@ -1,11 +1,31 @@
-import aiohttp
+from datetime import datetime
+
+import discord
 from discord.ext import commands
-import random
+import asyncpraw
+
+import config
 
 
 class Memes(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.reddit = None
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        if self.reddit is None:
+            self.reddit = asyncpraw.Reddit(
+                client_id=config.reddit_client_id,
+                client_secret=config.reddit_client_secret,
+                username=config.reddit_username,
+                password=config.reddit_password,
+                user_agent=(
+                    f"python:{self.bot.user.name}:v0.1.0 "
+                    f"(by u/{config.reddit_username})"
+                ),
+                requestor_kwargs={"session": self.bot.http_session},
+            )
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -25,27 +45,41 @@ class Memes(commands.Cog):
             await reaction.message.add_reaction("<:faridsus:768530389685764127>")
 
     @commands.command()
-    async def meme(self, ctx, *category):
-        '''Trouve un meme sur le subreddit mentionné
+    async def meme(self, ctx, *, subreddit_name="physicsmemes"):
+        """Trouve un meme sur le subreddit mentionné
         (par défaut sur r/physicsmemes)
-        '''
-        if not category:
-            category = ["physicsmemes"]
+        """
+        subreddit = await self.reddit.subreddit(subreddit_name.lower())
+        await subreddit.load()
+        submission = await subreddit.random()
+        await submission.author.load()
 
-        subreddit = "".join(category).lower()
-        url = "http://www.reddit.com/r/" + subreddit + "/hot/.json?limit=50"
+        if subreddit.over18 or submission.over_18:
+            # we don't want NSFW posts here
+            print(f"Someone tried to look at NSFW memes:\n{submission.url}")
+            return await ctx.reply("https://i.redd.it/cah38y4p41f51.png")
 
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as request:
-                if request:
-                    json = await request.json()
-                else:
-                    await ctx.send("Je n'ai pas réussi")
-        posts = json["data"]["children"]
-        posts = list(filter(lambda post : post["data"]["thumbnail"] != "nsfw", posts))
-        post = random.choice(posts)
-        img = post["data"]["url"]
-        await ctx.send(img)
+        embed = discord.Embed(
+            color=0xFF4500,
+            title=submission.title,
+            url=f"https://www.reddit.com{submission.permalink}",
+            description=submission.selftext,
+            timestamp=datetime.utcfromtimestamp(submission.created_utc),
+        ).set_image(
+            url=submission.url,
+        ).set_author(
+            name=submission.author.name,
+            url=f"https://www.reddit.com/u/{submission.author.name}",
+            icon_url=submission.author.icon_img,
+        ).set_footer(
+            text="  •  ".join([
+                f"{submission.score} points",
+                f"{submission.num_comments} comments",
+                "Published on",
+            ]),
+        )
+
+        await ctx.reply(embed=embed)
 
 
 def setup(bot):
